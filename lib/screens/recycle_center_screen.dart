842fbf943808feec439ce0b0f194631072c82861
ShubhamVG <vgshubham49@gmail.com>
2024-11-09 17:03:35 +0530

    Fix text overflowing

--- lib/screens/recycle_center_screen.dart
+++ lib/screens/recycle_center_screen.dart
@@ -10,113 +10,127 @@ class RecycleCenterScreen extends StatelessWidget {
     const separationHeight = 20.0;
     const headerTextSize = 20.0;
 
+    final size = MediaQuery.sizeOf(context);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Recycle centers')),
       bottomNavigationBar: const MyBottomNavbar(selectedIndex: 1),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Map container and search button
-              ConstrainedBox(
-                constraints: const BoxConstraints(
-                  minHeight: 200.0,
-                  minWidth: double.infinity,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Map container and search button
+                SizedBox(
+                  height: size.height / 3.5,
+                  width: double.infinity,
+                  child: const Card.filled(color: Colors.green),
                 ),
-                child: const Card.filled(color: Colors.green),
-              ),
-              const SizedBox(height: 10.0),
-              ConstrainedBox(
-                constraints: const BoxConstraints(minWidth: double.infinity),
-                child: FilledButton(
-                  onPressed: () {},
-                  child: const Text('Search Nearby Centers'),
+                const SizedBox(height: 10.0),
+                SizedBox(
+                  width: double.infinity,
+                  child: FilledButton(
+                    onPressed: () {},
+                    child: const Text('Search Nearby Centers'),
+                  ),
                 ),
-              ),
 
-              const SizedBox(height: separationHeight),
+                const SizedBox(height: separationHeight),
 
-              // Recycling Tips
-              const Text(
-                'Recycling Tips',
-                style: TextStyle(
-                  fontSize: headerTextSize,
-                  fontWeight: FontWeight.bold,
+                // Recycling Tips
+                const Text(
+                  'Recycling Tips',
+                  style: TextStyle(
+                    fontSize: headerTextSize,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      decoration: const ShapeDecoration(
-                        color: Colors.grey,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        decoration: const ShapeDecoration(
+                          color: Colors.grey,
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5.0)),
+                          ),
                         ),
+                        height: 80.0,
+                        width: 80.0,
                       ),
-                      height: 80.0,
-                      width: 80.0,
-                    ),
-                    const SizedBox(width: 10.0),
-                    const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'The 4 R\'s',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Text('Discover tips to reduce waste at home'),
-                      ],
-                    )
-                  ],
+                      const SizedBox(width: 10.0),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'The 4 R\'s',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: size.width - 136,
+                            child: Text(
+                              'Discover tips to reduce waste at home',
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              const Divider(),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      decoration: const ShapeDecoration(
-                        color: Colors.grey,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                const Divider(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        decoration: const ShapeDecoration(
+                          color: Colors.grey,
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5.0)),
+                          ),
                         ),
+                        height: 80.0,
+                        width: 80.0,
                       ),
-                      height: 80.0,
-                      width: 80.0,
-                    ),
-                    const SizedBox(width: 10.0),
-                    const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Eco-Friendly Shopping',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Text('Choose products with recyclable packaging'),
-                      ],
-                    )
-                  ],
+                      const SizedBox(width: 10.0),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Eco-Friendly Shopping',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: size.width - 136,
+                            child: Text(
+                              'Choose products with recyclable packaging',
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              const Divider(),
-              const SizedBox(height: separationHeight),
+                const Divider(),
+                const SizedBox(height: separationHeight),
 
-              // Scan QR Code button
-              ConstrainedBox(
-                constraints: const BoxConstraints(minWidth: double.infinity),
-                child: FilledButton(
-                  onPressed: () {},
-                  child: const Text('Scan QR Code of Dispose Machine'),
+                // Scan QR Code button
+                ConstrainedBox(
+                  constraints: const BoxConstraints(minWidth: double.infinity),
+                  child: FilledButton(
+                    onPressed: () {},
+                    child: const Text('Scan QR Code of Dispose Machine'),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
