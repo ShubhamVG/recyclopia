d72ad9192573aa5cb671db8cd40eee04085b2d1d
ShubhamVG <vgshubham49@gmail.com>
2024-11-09 17:06:01 +0530

    Add kDebugMode check to always start on login screen in debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  final prefs = await SharedPreferences.getInstance();
+  final isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+
+  if (isLoggedIn && !kDebugMode) {
+    runApp(MyApp(prefs: prefs));
+  } else {
+    runApp(MyApp(prefs: prefs, toLogin: true));
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.prefs, this.toLogin = false});
+
+  final SharedPreferences prefs;
+  final bool toLogin;
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +37,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: toLogin ? LoginScreen(prefs: prefs) : const HomeScreen(),
     );
   }
 }
